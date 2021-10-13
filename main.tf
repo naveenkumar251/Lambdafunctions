506b6784a6e5ef07311aa2e808337d565a466d75
naveenkumar251 <91377442+naveenkumar251@users.noreply.github.com>
2021-10-13 16:59:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {                                                                                                                                                       
+  region = "us-east-1"                                                                                                                                                 
+}                                                                                                                                                                      
+                                                                                                                                                                       
+module "terraform-aws-lambda-function" {                                                                                                                               
+  source  = "naveenkumar251/Lambdafunctions"                                                                                                                          
+  version = "~> 0.5.0"                                                                                                                                                 
+                                                                                                                                                                       
+  function_name = "python-function-newu11"                                                                                                                             
+  description   = "Example Python Lambda Function that returns an HTTP response."                                                                                      
+  filename      = "my-deployment-package.zip"                                                                                                                          
+  runtime       = "python3.8"                                                                                                                                          
+  handler       = "main.lambda_handler"                                                                                                                                
+                                                                                                                                                                       
+                                                                                                                                                                       
+  timeout     = 30                                                                                                                                                     
+  memory_size = 128                                                                                                                                                    
+                                                                                                                                                                       
+  role_arn = "arn:aws:iam::059062539581:role/LambdaNewRoleU11"                                                                                                         
+}                                                                                                                                                                      
